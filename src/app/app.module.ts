2aaaadf533883fde660e1b359a2d0037fec3f998
Oussema Lahami <oussemalahami1995@gmail.com>
2023-04-29 10:26:08 +0200

    added tailwind to the project and basic layout for the pokeCard component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PokeCardComponent } from './poke-card/poke-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PokeCardComponent
   ],
   imports: [
     BrowserModule
